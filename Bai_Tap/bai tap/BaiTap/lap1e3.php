e39ce5fb21bdf1e2f1361937d81ed97d7f0db1b0
Zleozpham <45645698+Zleozpham@users.noreply.github.com>
2020-04-12 21:43:17 +0700

    Create lap1e3.php

---
+++ Bai_Tap/bai tap/BaiTap/lap1e3.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<?php
+		echo "Tên sinh viên: Đoàn Văn Nhật<br>";
+		echo "Lớp: LT0718A<br>";
+		echo "Môn học: Lập trình web với php";
+	?>
+</body>
+</html>
