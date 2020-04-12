b02ed7b11cf04385315ccbf4f855984816ae50bc
Zleozpham <45645698+Zleozpham@users.noreply.github.com>
2020-04-12 21:42:51 +0700

    Create bai2.php

---
+++ Bai_Tap/bai tap/bai2/bai2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		php 2
+	</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<p align="center"> Xay dung chuong trinh nhap vao 3 canh cua tam giac hay kiem tra ham du lieu dau vao co phai la 3 canh cua 1 tam giac khong?La tam giac gi? Tinh chu vi va dien tich</p>
+	<form name="frmcheck" action="check2.php" method="get">
+		<input type="number" name="a" value="">
+		<input type="number" name="b" value="">
+		<input type="number" name="c" value="">
+		<input type="submit" name="Check tam giac" value="check2">
+
+
+	</form>
+
+</body>
+</html>
