5ce1454f794f7ea856836d03e450a269d3748ae4
Zleozpham <45645698+Zleozpham@users.noreply.github.com>
2020-04-12 21:43:14 +0700

    Create lap1e1.php

---
+++ Bai_Tap/bai tap/BaiTap/lap1e1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<?php
+		echo "<font color='blue' size=25px ><p align=center><b>Lập trình PHP<b></p></font>";
+	?>
+</body>
+</html>
