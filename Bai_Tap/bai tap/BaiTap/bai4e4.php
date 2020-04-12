18a6259fb2b32550dc45926b666caf5ea5e3cbd0
Zleozpham <45645698+Zleozpham@users.noreply.github.com>
2020-04-12 21:43:09 +0700

    Create bai4e4.php

---
+++ Bai_Tap/bai tap/BaiTap/bai4e4.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<?php
+		$s1=0;
+		for($i=1; $i<=10; $i++){
+			$s1=2*$i;
+			echo"2*".$i."=".$s1."<br>";
+		}
+		echo "<br><br>";
+		$s2=0;
+		for ($i=3; $i<=10 ; $i++) { 
+			$s2=3*$i;
+			echo"3*".$i."=".$s2."<br>";
+		}
+		for ($i=4; $i <=10 ; $i++) { 
+			$s=4*$i;
+			echo "4*";
+		}
+	?>
+</body>
+</html>
