8e3b5af5ff7bd66d3293b39174b766bab68d3978
Zleozpham <45645698+Zleozpham@users.noreply.github.com>
2020-04-12 21:43:07 +0700

    Create bai3lienket.php

---
+++ Bai_Tap/bai tap/BaiTap/bai3lienket.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<form action="bai3.php" method="GET">
+		<input type="number" name="n" placeholder="nhập n" style="width: 100px;height: 50px;" autocomplete="off" autofocus="">
+			<br><br>
+			<q><i>Nhập số từ 5-100 ý :3</i></q>
+			<br><br>
+		<input type="submit" name="btn_submit" value="Tính toán">
+	</form>
+</body>
+</html>
