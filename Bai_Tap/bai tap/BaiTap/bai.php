f1a71b1ad34ca045ff0b94fa10b1b7f0bfb813b3
Zleozpham <45645698+Zleozpham@users.noreply.github.com>
2020-04-12 21:43:01 +0700

    Create bai.php

---
+++ Bai_Tap/bai tap/BaiTap/bai.php
@@ -0,0 +1,12 @@
+<?php
+	$a=$_GET["so1"] ;
+	$b=$_GET["so2"];
+	$tong=$a+$b;
+	echo "tong".$a.$b."=".$tong."<br>";
+	$hieu=$a-$b;
+	echo "hieu $a-$b=$hieu <br>";
+	$tich=$a*$b;
+	echo "tich $a*$b=$tich<br>";
+	$thuong=$a/$b;
+	echo "thuong $a/$b=$thuong";
+?>
