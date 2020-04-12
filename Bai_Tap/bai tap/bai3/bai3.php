1c0d9ba9cb75b1954a433f02dac0ce27d2687d62
Zleozpham <45645698+Zleozpham@users.noreply.github.com>
2020-04-12 21:42:56 +0700

    Create bai3.php

---
+++ Bai_Tap/bai tap/bai3/bai3.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		pHp
+	</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<p align="center">Xay dung form nhap vao so nguiyen n neu n khong thuoc [5;10] thi bat nhap lai neu n thuoc [5;10] thi tinh <br>S1=1+2+...+n<br> S2=1^2+2^2+3^2+...+n^2<br>S3=1/3+2/5+...+n/2n+1</p>
+	<form method="get" name="frmcheck" action="check3.php" >
+		<input type="number" name="a" value="">
+		<input type="submit" name="tinh toan S" value="check3">
+		
+	</form>
+</body>
+</html>
