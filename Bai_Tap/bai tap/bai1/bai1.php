8116bedf1e81a1bb1e93d46e07f5878bcba0d5f4
Zleozpham <45645698+Zleozpham@users.noreply.github.com>
2020-04-12 21:42:45 +0700

    Create bai1.php

---
+++ Bai_Tap/bai tap/bai1/bai1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		pHp 2
+	</title>
+
+	<meta charset="utf-8">
+</head>
+<body>
+		<p align="center"> Nhap vao 3 so a b c tuong ung phuong trinh bac 2 ax^2+bx+c=0 Hay kiem tra du lieu dau vao roi giai chuong trinh va in ket qua ra man hinh</p>
+		<form name="frmcheck" action="check1.php" method="get">
+			<input type="number" name="a" value="">
+			<input type="number" name="b" value="">
+			<input type="number" name="c" value="">
+			<input type="submit" name="btncheck" value="check1">
+			<input type="reset" name="reset" value="reset">
+		</form>
+</body>
+</html>
