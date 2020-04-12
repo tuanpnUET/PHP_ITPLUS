53b4b17e1dfc34158a871eb0e7b9fa9a0a38d6c3
Zleozpham <45645698+Zleozpham@users.noreply.github.com>
2020-04-12 21:43:11 +0700

    Create baiorm.php

---
+++ Bai_Tap/bai tap/BaiTap/baiorm.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<form name="" action="bai.php" method="get">
+			Số thứ nhất: <input type="text" name="so1" value="8"><br>
+			Số thứ hai: <input type="text" name="so2" value="10"><br>
+			<input type="submit" name="" value="tinhtoan">
+	</form>
+</body>
+</html>
